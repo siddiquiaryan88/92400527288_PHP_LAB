a05e21b0f5250e5691342da13221b5bd03a25310
Aryan Siddiqui <siddiquiaryan749@gmail.com>
2026-06-23 12:03:40 +0530

    Add files via upload

---
+++ lab.2.1.php
@@ -0,0 +1,10 @@
+<?php
+function collegeInfo($college, $course, $year)
+{
+    echo "College Name: $college<br>";
+    echo "Course Name: $course<br>";
+    echo "Academic Year: $year";
+}
+
+collegeInfo("ABC College", "BCA", "2025-2026");
+?>
